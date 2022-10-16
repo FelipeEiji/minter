97f6bb9f3a783e141a212cea5e7a191fea927447
Felipe Eiji Maruyama <felipe-eiji@hotmail.com>
2022-10-16 02:02:45 -0300

    feat: implements user authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createClient, configureChains, defaultChains, WagmiConfig } from "wagmi";
+import { extendTheme } from "@chakra-ui/react";
+import { publicProvider } from "wagmi/providers/public";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const { provider, webSocketProvider } = configureChains(defaultChains, [publicProvider()]);
 
-export default MyApp
+const client = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: true,
+});
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
+
+const MyApp = ({ Component, pageProps }: AppProps<any>) => {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <WagmiConfig client={client}>
+        <SessionProvider session={pageProps.session} refetchInterval={0}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </WagmiConfig>
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
