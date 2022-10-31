e620ab0c7738bbba1ae59ef6cb53d3ff6c935157
Felipe Eiji Maruyama <felipe-eiji@hotmail.com>
2022-10-30 23:21:39 -0300

    feat: create update-market-item cloud function

--- cloud/update-market-item.js
+++ cloud/update-market-item.js
@@ -1,12 +1,6 @@
 Moralis.Cloud.afterSave("MarketItemSoldEvent", async (request) => {
-    // logger.info("Request:");
-    // logger.info(JSON.stringify(request));
-    // logger.info("Request object:");
-    // logger.info(JSON.stringify(request.object));
-    // logger.info("Request object ItemId:");
-    // logger.info(JSON.stringify(request.object.get("itemId")));
     const query = new Moralis.Query("MarketItem");
     const marketItem = await query.equalTo("itemId", request.object.get("itemId")).first();
-    logger.info("MarketItem:");
-    logger.info(JSON.stringify(marketItem));
+    marketItem.set("sold", true);
+    await marketItem.save();
 });
